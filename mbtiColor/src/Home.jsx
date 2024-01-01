833a1731f014b90cf1c0f72b54242fa4bffda531
wjdtj9656 <wjdtj9656@naver.com>
2024-01-01 19:56:58 +0900

    mock + home.jsx layout

--- mbtiColor/src/Home.jsx
+++ mbtiColor/src/Home.jsx
@@ -1,4 +1,30 @@
+import { Link } from "react-router-dom";
+import mock from "./mock.json";
+import { useState } from "react";
 function Home() {
-  return <div>Home</div>;
+  const [filter, setFilter] = useState(null);
+  return (
+    <div>
+      <header>
+        <h1>MBTI별 좋아하는 컬러</h1>
+        {filter && (
+          <div onClick={() => setFilter(null)}>
+            {filter}
+            <img src="/images/x.svg" alt="필터 삭제" />
+          </div>
+        )}
+        <div>
+          <Link to="/new"> + 새 컬러 등록하기</Link>
+          <ul>
+            {mock.results.map((item) => (
+              <li key={item.id} onClick={() => setFilter(item.mbti)}>
+                {item.id} {item.mbti} --- {item.colorCode}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </header>
+    </div>
+  );
 }
 export default Home;
