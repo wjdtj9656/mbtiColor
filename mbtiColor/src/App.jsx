0736706bb7d0ff5966019c24818ccb9b0a7e6dba
wjdtj9656 <wjdtj9656@naver.com>
2024-01-01 19:41:44 +0900

    init react

---
+++ mbtiColor/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import New from "./New";
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="new" element={<New />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
